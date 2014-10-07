f63c99e8d913e8c226875fc1275e0b2905b13c28
Paul Moore <p.f.moore@gmail.com>
2014-10-07 12:27:50 +0100

    Tox and coverage integration

---
+++ toxcov/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='sample',
+    version='0.1',
+    description='Test package',
+    long_description='Test package',
+    author='Paul Moore',
+    author_email='p.f.moore@gmail.com',
+    url='https://github.com/pfmoore/sandbox',
+    packages=['sample',],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+    ],
+)
---
+++ toxcov/tests/test_sample.py
@@ -0,0 +1,4 @@
+import sample
+
+def test_sample():
+    assert sample.does_something_version_specific()
